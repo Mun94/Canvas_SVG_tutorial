6dc57ee37ae118d817695fc8b64178a33463d24a
Mun94 <seongwoo720@gmail.com>
2021-08-23 10:52:03 +0900

    repactor: variable name

--- bullet ui/js/script.js
+++ bullet ui/js/script.js
@@ -9,88 +9,96 @@ class Color {
     constructor() {
         this.background = '#393C43'; // 배경 색
 
-        this.normal   = '#4D8BD5'; // 정상 색
-        this.warning  = '#B8A605'; // 경고 색
-        this.critical = '#B40E0A'; // 심각 색
+        this.nor = '#4D8BD5'; // 정상 색
+        this.war = '#B8A605'; // 경고 색
+        this.cri = '#B40E0A'; // 심각 색
 
         this.basicFont = '#C6C9CD'; // 기본 폰트 색
 
         this.basicLine = '#C6C9CD'; // 라인 색
     };
 };
-
+const color = new Color();
 class TimeCondition {
     constructor(data) {
-        this.normalCondition   = data.colorByRunTime >= 1 && data.colorByRunTime < 3;
-        this.warningCondition  = data.colorByRunTime >= 3 && data.colorByRunTime <= 5;
-        this.criticalCondition = data.colorByRunTime > 5  && data.colorByRunTime <= 10;
+        this.norCondition = data.colorByRunTime >= 1 && data.colorByRunTime < 3;
+        this.warCondition = data.colorByRunTime >= 3 && data.colorByRunTime <= 5;
+        this.criCondition = data.colorByRunTime > 5  && data.colorByRunTime <= 10;
+    };
+};
+const timeCondition = (data) => new TimeCondition(data);
+class Position {
+    constructor(aniPosition) {
+        this.canvasW = canvas.width;
+        this.canvasH = canvas.height;
+
+        this.bulletPathY = 240;
+
+        this.reqX  = this.canvasW / 3;
+        this.excuX = this.canvasW * ( 2 / 3 );
+
+        this.startY = 120;
+        this.startX = 0;
+
+        if(aniPosition) {
+            // 에니메이션 수행에 필요한 조건
+            this.arcDiameter = 20;
+
+            this.area  = this.canvasW / 3; // left; mid; right 구역 당 width
+
+            this.reqEndX    = this.reqX - this.arcDiameter;
+            this.excuStartX = this.reqX + this.arcDiameter;
+            this.excuEndY   = this.canvasH - this.arcDiameter;
+            this.resStartX  = this.excuX + this.arcDiameter
+        };
     };
 };
 
-const color = new Color();
-const timeCondition = (data) => new TimeCondition(data);
-class Size {
+class FontPosition extends Position {
     constructor() {
-        this.canvasW = canvas.width;
-        this.canvasH = canvas.height;
+        const needAniPosition = false;
+        super(needAniPosition);
 
         this.startRectX = 0;
         this.startRectY = 0;
 
-        this.arcDiameter = 20;
-
-        this.bulletPathY = 240;
-
-        this.startY = 0;
-        this.startX = 0;
-
-        this.area = this.canvasW / 3; // left; mid; right 구역 당 width
-        this.left = this.canvasW / 3;
-        this.mid  = this.canvasW * ( 2 / 3 );
-
-        this.leftEndX  = this.left - this.arcDiameter;
-        this.startMidX = this.left + this.arcDiameter;
-        this.endMidX   = this.mid - this.arcDiameter;
-
-        this.startMidY = 120;
-        this.endMidY   = this.canvasH - this.arcDiameter;
-
-        this.startRightX = this.mid + this.arcDiameter
-
         // 글자 영역에 공통으로 필요한 조건
         this.countTitleGap = 35;
 
-        // left 글자 영역에 필요한 위치 조건
+        // req 글자 영역에 필요한 위치 조건
         this.totalCountX = 30; 
-        this.leftTotalFontY = 80;
-        this.reqCountX = 130;
-        this.leftReqFontY = 170;
+        this.totalFontY  = 80;
+        this.reqCountX   = 130;
+        this.reqFontY    = 170;
 
-        // mid 글자 영역에 필요한 위치 조건
-        this.midFontY = 80;
+        // excu 글자 영역에 필요한 위치 조건
+        this.excuFontY = 80;
+        this.norWarCriGap = 150;
 
-        this.normalCountX   = 320;
-        this.warningCountX  = 420;
-        this.criticalCountX = 520;
+        this.norCountX   = (this.canvasW / 3) + 20;
+        this.warCountX  = this.norCountX + this.norWarCriGap;
+        this.criCountX = this.warCountX + this.norWarCriGap;
 
-        // right 글자 영역에 필요한 위치 조건
-        this.resCountX = 690;
-        this.rightResFontY = 170;
+        // res 글자 영역에 필요한 위치 조건
+        this.resCountX = this.canvasW * ( 3 / 4);
+        this.redFontY = 170;
     };
 };
-
-class SetDatas extends Size{
+class SetDatas extends Position{
     constructor(){
-        super();
-        const dataPerSec = 20; // 1 초 당 20 개
-        const req = (dataPerSec / 5); // 0.2초에 한 번씩 요청이 발생 함(4개)
+        const needAniPosition = true;
+        super(needAniPosition);
+
+        const dataPerSec = 5; // 1 초 당 20 개
+        const reqGap = 1;
+        const req = (dataPerSec / reqGap); // 0.2초에 한 번씩 요청이 발생 함(4개)
 
         this.datas     = [];
         this.dataPerReq = req; // 0.2초 마다 발생 할 요청에 생성 될 데이터 수
 
         // this.addDatas();
-        reqCount = reqPerSec;
-        setInterval(() => this.addDatas(), (1000 / 5)); // 지금 0.2 초당 4개 생성 됨
+        reqCount = dataPerSec;
+        setInterval(() => this.addDatas(), (1000 / reqGap)); // 지금 0.2 초당 4개 생성 됨
     };
 
     addDatas(){
@@ -109,69 +117,68 @@ class SetDatas extends Size{
         };
     };
 };
-
 const setDatas = new SetDatas();
 
 class Animation extends SetDatas {
     constructor(){
         super();
         
-        this.middleDatas = [];
-        this.rightDatas = [];
+        this.excuDatas = []; // 실행 될 데이터 배열
+        this.resDatas = [];
     };
 
-    animationLeft() {
+    reqAni() {
         for(let data of this.datas) {
-            this.createBullet('#4D8BD5', data, 'left'); // blue
+            this.createBullet(color.nor, data, 'reqArea'); // blue
 
             const {colorByRunTime, runTime, speed} = data;
 
-            if(data.x > this.leftEndX) {
-                this.middleDatas.push({
+            if(data.x > this.reqEndX) {
+                this.excuDatas.push({
                     colorByRunTime,
                     runTime,
 
-                   mx: this.startMidX + (Math.random() * (this.area - this.arcDiameter - this.arcDiameter)),
-                   my: this.startMidY + (Math.random() * (this.endMidY - this.startMidY)),
-                   mxSpeed: Math.sign(Math.random() - 0.5) > 0 ? speed : -speed,
-                   mySpeed: Math.sign(Math.random() - 0.5) > 0 ? speed : -speed,
+                   mx: this.excuStartX + (Math.random() * (this.area - this.arcDiameter - this.arcDiameter)),
+                   my: this.startY + (Math.random() * (this.excuEndY - this.startY)),
+                   mxSpeed: Math.sign(Math.random() - 0.5) * speed,
+                   mySpeed: Math.sign(Math.random() - 0.5) * speed,
                 });
 
-                dataCount = this.middleDatas;
-                this.datas = this.datas.filter(data => data.x < this.leftEndX); // 원의 지름 뺌
+                dataCount = this.excuDatas;
+                this.datas = this.datas.filter(data => data.x < this.reqEndX); // 원의 지름 뺌
             };
         };
     };
 
-    animationMiddle() {
-        if(!this.middleDatas.length) { return; };
+    excuAni() {
+        if(!this.excuDatas.length) { return; };
 
         function bounce(data) { // 일반 함수에 bind 안 사용하고 화살표 함수 사용해도 됨
-            if(data.mx >= (this.mid - this.arcDiameter)) {
+            if(data.mx >= (this.excuX - this.arcDiameter)) {
                 data.mxSpeed = -data.mxSpeed;
             };
             
-            if(data.mx <= this.startMidX) {
+            if(data.mx <= this.excuStartX) {
                 data.mxSpeed = Math.abs(data.mxSpeed);
             };
     
-            if(data.my >= this.endMidY) {
+            if(data.my >= this.excuEndY) {
                 data.mySpeed = -data.mySpeed
             };
             
-            if(data.my <= (this.startMidY + this.arcDiameter)) {
+            if(data.my <= (this.startY + this.arcDiameter)) {
                 data.mySpeed = Math.abs(data.mySpeed);
             };
         }
     
-        for(let data of this.middleDatas) {
-            this.createBulletByRunTime(data, 'mid', bounce.bind(this));
+        for(let data of this.excuDatas) {
+            this.createBulletByRunTime(data, 'excuArea', bounce.bind(this));
         };
     };
 
-    animationRight() {    
-        for(let data of this.rightDatas) {
-            this.createBulletByRunTime(data, 'right');
+    resAni() {    
+        for(let data of this.resDatas) {
+            this.createBulletByRunTime(data, 'resArea');
         };
     };
 
@@ -181,13 +188,13 @@ class Animation extends SetDatas {
         ctx.fillStyle = color;
 
         switch(area) {
-            case 'left':
+            case 'reqArea':
                 ctx.arc(data.x += data.speed , data.y, this.arcDiameter, 0, Math.PI * 2);
                 break;
-            case 'mid':
+            case 'excuArea':
                 ctx.arc(data.mx += data.mxSpeed , data.my += data.mySpeed, this.arcDiameter, 0, Math.PI * 2);
                 break;
-            case 'right':
+            case 'resArea':
                 ctx.arc(data.rx += data.speed, data.ry, this.arcDiameter, 0, Math.PI * 2);
                 break;
             default:
@@ -198,30 +205,30 @@ class Animation extends SetDatas {
     };
 
     createBulletByRunTime(data, area, bounceFn) {
-        if(timeCondition(data).normalCondition) { // 1에서 3초
-            this.createBullet(color.normal, data,  area); // blue
+        if(timeCondition(data).norCondition) { // 1에서 3초
+            this.createBullet(color.nor, data,  area); // blue
 
             bounceFn && bounceFn(data);
         }; 
 
-        if(timeCondition(data).warningCondition) { // 3에서 5초
-            this.createBullet(color.warning, data, area); // yellow
+        if(timeCondition(data).warCondition) { // 3에서 5초
+            this.createBullet(color.war, data, area); // yellow
 
             bounceFn && bounceFn(data);
         };
 
-        if(timeCondition(data).criticalCondition) { // 5에서 10초
-            this.createBullet(color.critical, data, area); // red
+        if(timeCondition(data).criCondition) { // 5에서 10초
+            this.createBullet(color.cri, data, area); // red
 
             bounceFn && bounceFn(data);
         };
     };
 
     excuteRuntime() {
-        if(!this.middleDatas.length) { return; };
-        this.middleDatas.forEach(data => data.runTime--);
+        if(!this.excuDatas.length) { return; };
+        this.excuDatas.forEach(data => data.runTime--);
 
-        const runTimeEndBullets = this.middleDatas.filter(data => 
+        const runTimeEndBullets = this.excuDatas.filter(data => 
             data.runTime <= 0
         );
 
@@ -232,23 +239,24 @@ class Animation extends SetDatas {
         if(longestRunTime) {
             const {colorByRunTime, runTime, mxSpeed} = longestRunTime;
 
-            this.rightDatas.push({colorByRunTime, runTime, rx: this.startRightX, ry: this.bulletPathY, speed: Math.abs(mxSpeed)});
+            this.resDatas.push({colorByRunTime, runTime, rx: this.resStartX, ry: this.bulletPathY, speed: Math.abs(mxSpeed)});
         };
          
-        this.middleDatas = this.middleDatas.filter(data => data.runTime > 0);
-        dataCount = this.middleDatas;
-        // console.log(this.rightDatas)
-        // console.log(this.middleDatas.map(a => a.runTime))
+        this.excuDatas = this.excuDatas.filter(data => data.runTime > 0);
+        dataCount = this.excuDatas;
+        // console.log(this.resDatas)
+        // console.log(this.excuDatas.map(a => a.runTime))
     };
 
     render() {
-        this.animationLeft();
-        this.animationMiddle();
-        this.animationRight();
+        this.reqAni();
+        this.excuAni();
+        this.resAni();
     };
 };
+const animation  = new Animation();
 
-class Background extends Size{
+class Background extends FontPosition{
     constructor(){
         super();
     };
@@ -261,9 +269,9 @@ class Background extends Size{
 
         this.line();
 
-        this.leftCount();
-        this.midCount();
-        this.rightCount();
+        this.reqCount();
+        this.excuCount();
+        this.resCount();
 
         ctx.stroke();
     };
@@ -271,77 +279,76 @@ class Background extends Size{
     line() {
         ctx.strokeStyle = color.basicLine;
 
-        ctx.moveTo(this.left   , this.startMidY);
-        ctx.lineTo(this.left   , this.canvasH);
-        ctx.moveTo(this.mid    , this.startMidY);
-        ctx.lineTo(this.mid    , this.canvasH);
-        ctx.moveTo(this.startX , this.bulletPathY);
-        ctx.lineTo(this.left   , this.bulletPathY);
-        ctx.moveTo(this.mid    , this.bulletPathY);
-        ctx.lineTo(this.canvasW, this.bulletPathY);
+        ctx.moveTo(this.reqX     , this.startY);
+        ctx.lineTo(this.reqX     , this.canvasH);
+        ctx.moveTo(this.excuX    , this.startY);
+        ctx.lineTo(this.excuX    , this.canvasH);
+        ctx.moveTo(this.startX   , this.bulletPathY);
+        ctx.lineTo(this.reqX     , this.bulletPathY);
+        ctx.moveTo(this.excuX    , this.bulletPathY);
+        ctx.lineTo(this.canvasW  , this.bulletPathY);
     };
 
-    leftCount() {
-        const totalCount = this.count().normal + this.count().warning + this.count().critical;
+    reqCount() {
+        const totalCount = this.count().nor + this.count().war + this.count().cri;
 
         ctx.font = '30px Arial';
 
         ctx.fillStyle = color.basicFont;
-        ctx.fillText('현재 전체 건수', this.totalCountX + this.countTitleGap, this.leftTotalFontY);
-        ctx.fillText(totalCount, this.totalCountX, this.leftTotalFontY);
+        ctx.fillText('현재 전체 건수', this.totalCountX + this.countTitleGap + 13, this.totalFontY);
+        ctx.fillText(totalCount, this.totalCountX, this.totalFontY);
         
         ctx.font = '25px Arial';
 
-        ctx.fillText('요청/초', this.reqCountX, this.leftReqFontY);
-        ctx.fillText(reqCount, this.reqCountX - this.countTitleGap, this.leftReqFontY)
+        ctx.fillText('요청/초', this.reqCountX, this.reqFontY);
+        ctx.fillText(reqCount, this.reqCountX - this.countTitleGap, this.reqFontY)
 
     };
 
-    midCount() {
+    excuCount() {
         ctx.font = '25px Arial';
 
         ctx.fillStyle = color.basicFont;
-        ctx.fillText('정상', this.normalCountX + this.countTitleGap, this.midFontY);
-        ctx.fillText('경고', this.warningCountX + this.countTitleGap, this.midFontY);
-        ctx.fillText('심각', this.criticalCountX + this.countTitleGap, this.midFontY);
+        ctx.fillText('정상', this.norCountX + this.countTitleGap, this.excuFontY);
+        ctx.fillText('경고', this.warCountX + this.countTitleGap, this.excuFontY);
+        ctx.fillText('심각', this.criCountX + this.countTitleGap, this.excuFontY);
 
-        ctx.fillStyle = color.normal;
-        ctx.fillText(this.count().normal, this.normalCountX, this.midFontY);
+        ctx.fillStyle = color.nor;
+        ctx.fillText(this.count().nor, this.norCountX, this.excuFontY);
         
-        ctx.fillStyle = color.warning;
-        ctx.fillText(this.count().warning, this.warningCountX, this.midFontY);
+        ctx.fillStyle = color.war;
+        ctx.fillText(this.count().war, this.warCountX, this.excuFontY);
    
-        ctx.fillStyle = color.critical;
-        ctx.fillText(this.count().critical, this.criticalCountX, this.midFontY);
+        ctx.fillStyle = color.cri;
+        ctx.fillText(this.count().cri, this.criCountX, this.excuFontY);
     };
 
-    rightCount() {
+    resCount() {
         ctx.font = '25px Arial';
 
         ctx.fillStyle = color.basicFont;
-        ctx.fillText('응답/초', this.resCountX, this.rightResFontY);
-        ctx.fillText(resCount, this.resCountX + (this.countTitleGap * 3), this.rightResFontY);
+        ctx.fillText('응답/초', this.resCountX, this.redFontY);
+        ctx.fillText(resCount, this.resCountX + (this.countTitleGap * 3), this.redFontY);
     };
 
     count() {
-        const normal = (dataCount || []).filter(data => 
-                timeCondition(data).normalCondition
+        const nor = (dataCount || []).filter(data => 
+                timeCondition(data).norCondition
             ).length;
 
-        const warning = (dataCount || []).filter(data => 
-                timeCondition(data).warningCondition
+        const war = (dataCount || []).filter(data => 
+                timeCondition(data).warCondition
             ).length;
         
-        const critical = (dataCount || []).filter(data => 
-                timeCondition(data).criticalCondition
+        const cri = (dataCount || []).filter(data => 
+                timeCondition(data).criCondition
             ).length;
         
-        return { normal, warning, critical };
+        return { nor, war, cri };
     };
 };
-
-const animation  = new Animation();
 const background = new Background();
+
 setInterval(() => animation.excuteRuntime(), 1000);
 
 const init = () => {
@@ -389,7 +396,7 @@ let aa = 0;
 let topss = 0;
 const main = () => {
     ctx2.clearRect(0, 0, canvas2.width, canvas2.height)
-    // const ty = snow.y += snow.size * 0.5;
+    // const ty = snow.y += snow.Position * 0.5;
     ctx2.beginPath();
     ctx2.fillStyle = 'red';
     ctx2.arc(topss++ , 120, 20, 0, Math.PI * 2);
