4f1dde7c921942a20c5054f76737096c4f31361f
Mun94 <seongwoo720@gmail.com>
2021-09-03 17:31:51 +0900

    add: controll paddle by mouse

--- canvas/game/brick out game/js/script.js
+++ canvas/game/brick out game/js/script.js
@@ -91,8 +91,17 @@ const script = () => {
         };
     };
 
+    const mouseMoveHandler = e => {
+        const relativeX = e.clientX - canvas.offsetLeft;
+
+        if(relativeX > 0 && relativeX < canvas.width) {
+            paddleX = relativeX - paddleWidth / 2;
+        };
+    };
+
     document.addEventListener('keydown', keyDownHandler, false);
     document.addEventListener('keyup', keyUpHandler, false);
+    document.addEventListener('mousemove', mouseMoveHandler, false);
 
     const drawPaddle = () => {
         if(rightPressed && paddleX < canvas.width - paddleWidth) {
