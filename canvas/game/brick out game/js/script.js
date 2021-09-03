80afcd624a5db53656103e24e7b39b7a5c0ce416
Mun94 <seongwoo720@gmail.com>
2021-09-03 16:28:10 +0900

    add: paddle and keyboard controll

--- canvas/game/brick out game/js/script.js
+++ canvas/game/brick out game/js/script.js
@@ -9,6 +9,44 @@ const script = () => {
 
     const ballRadius = 10;
 
+    const paddleHeight = 10;
+    const paddleWidth = 75;
+    let paddleX = (canvas.width - paddleWidth) / 2;
+    let rightPressed = false;
+    let leftPressed = false;
+
+    const keyDownHandler = e => {
+        if(e.keyCode === 39) {
+            rightPressed = true;
+        } else if(e.keyCode === 37) {
+            leftPressed = true;
+        };
+    };
+
+    const keyUpHandler = e => {
+        if(e.keyCode === 39) {
+            rightPressed = false;
+        } else if(e.keyCode === 37) {
+            leftPressed = false;
+        };
+    };
+
+    document.addEventListener('keydown', keyDownHandler, false);
+    document.addEventListener('keyup', keyUpHandler, false);
+
+    const drawPaddle = () => {
+        if(rightPressed && paddleX < canvas.width - paddleWidth) {
+            paddleX += 7;
+        } else if(leftPressed && paddleX > 0) {
+            paddleX -= 7;
+        };
+
+        ctx.beginPath();
+        ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+        ctx.fillStyle = '#0095DD';
+        ctx.fill();
+    };
+
     const drawBall = () => {
         ctx.beginPath();
         ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
@@ -20,6 +58,7 @@ const script = () => {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         drawBall();
+        drawPaddle();
 
         if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
             dx = -dx;
